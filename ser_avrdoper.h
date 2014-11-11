da3c8b399cfeaf5117f48fc75eb839ad32387483
sergey-raevskiy <ccik@inbox.ru>
2014-11-12 03:49:29 +0400

    Inital commit of ser_avrdoper.c / ser_avrdoper.h.

---
+++ ser_avrdoper.h
@@ -0,0 +1,16 @@
+/*
+ * Name: ser_avrdoper.h
+ * Project: avrdude
+ * Author: Christian Starkjohann, Objective Development Software GmbH
+ * Creation Date: 2006-10-18
+ * Tabsize: 4
+ * License: GNU General Public License.
+ * Revision: $Id$
+ */
+
+#ifndef __ser_avrdoper_h_included__
+#define __ser_avrdoper_h_included__
+
+extern struct serial_device avrdoper_serdev;
+
+#endif /* __ser_avrdoper_h_included__ */
